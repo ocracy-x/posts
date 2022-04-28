2661cb7e7e6e532945dbb16a49e44167bde9d07b
William Z <williazz@usc.edu>
2022-04-28 10:24:03 -0500

    Installed mocha and chai and passed sample test

---
+++ test/repos/comments.test.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('CommentsRepo', () => {
+	describe('Sample', () => {
+		it('should pass a sample test', () => {
+			expect(true).to.equal(true);
+		});
+	});
+});
