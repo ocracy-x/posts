617e83c54aea33651d4bcfe42a7316adefc0ea44
William Z <williazz@usc.edu>
2022-05-03 13:22:48 -0500

    Saving progress in controller, fixing broken firestore repo

--- src/communities/communities.controller.ts
+++ src/communities/communities.controller.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { Request, Response, NextFunction } from 'express';
-import { CommunitiesRepo } from './communities.repo';
+import { CommunitiesRepo, Community } from './communities.repo';
 
 import {
 	interfaces,
@@ -8,8 +8,11 @@ import {
 	httpGet,
 	request,
 	response,
+	httpPost,
+	queryParam,
 } from 'inversify-express-utils';
 import { inject } from 'inversify/lib/annotation/inject';
+import { resolve } from 'path';
 
 @controller('/v1/communities')
 export class CommunitiesController implements interfaces.Controller {
@@ -18,12 +21,33 @@ export class CommunitiesController implements interfaces.Controller {
 	) {}
 
 	@httpGet('/')
-	public async get(@request() req: Request, @response() res: Response) {
+	private async get(@request() req: Request, @response() res: Response) {
 		try {
 			const data = await this.communitiesRepo.getAll();
 			res.send(data);
 		} catch (err) {
 			res.status(500).send(err);
 		}
+	}
+
+	@httpPost('/')
+	private async create(@request() req: Request, @response() res: Response) {
+		try {
+			const doc = new Community('hello');
+			const data = await this.communitiesRepo.create(doc);
+			res.status(201).send(data);
+		} catch (err) {
+			res.status(500).send(err);
+		}
+	}
+
+	@httpGet('/paginate')
+	private async paginate(
+		@queryParam('start') start: number,
+		@queryParam('count') count: number,
+		@response() res: Response,
+	) {
+		const str = `start=${start}, count=${count}`;
+		res.status(201).send(str);
 	}
 }
--- src/communities/communities.repo.ts
+++ src/communities/communities.repo.ts
@@ -1,8 +1,24 @@
-import { Firestore } from '@google-cloud/firestore';
-import { Repo } from '../repo';
+import {
+	DocumentData,
+	DocumentSnapshot,
+	Firestore,
+} from '@google-cloud/firestore';
+import { rejects } from 'assert';
+import { Repo } from '../util';
 
 export class Community {
 	constructor(readonly name: string) {}
+	toJson(): Object {
+		return {
+			name: this.name,
+		};
+	}
+
+	static fromJson(json: Map<string, string>): Community {
+		const name = json.get('name');
+		if (name == null) throw Error('Community requires name');
+		return new Community(name);
+	}
 }
 
 export abstract class CommunitiesRepo extends Repo<Community> {
@@ -12,13 +28,24 @@ export abstract class CommunitiesRepo extends Repo<Community> {
 }
 
 export class CommunitiesFirestore extends CommunitiesRepo {
+	private converter = {
+		toFirestore(community: Community): DocumentData {
+			return community.toJson();
+		},
+		fromFirestore(snapshot: DocumentSnapshot): Community {
+			const data = snapshot.data();
+			if (!data) throw Error();
+			return new Community(data.name);
+		},
+	};
 	private store = new Firestore().collection(this.collection);
 
 	getAll(): Promise<Community[]> {
 		throw new Error('Method not implemented.');
 	}
-	create(item: Community): Promise<Community> {
-		throw new Error('Method not implemented.');
+	async create(item: Community): Promise<Community> {
+		await this.store.add(item);
+		return item;
 	}
 
 	update(item: Community): Promise<Community> {
