9e286523dae8aad7ce25a918b9aa627eb7c469d4
William Z <williazz@usc.edu>
2022-05-08 11:35:38 -0400

    Tested basic getAll method on profiles

---
+++ test/profiles.test.ts
@@ -0,0 +1,27 @@
+import chai, { should } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../src/app';
+import { Profile } from '../src/profiles/profiles.repo';
+
+chai.use(chaiHttp);
+should();
+
+describe('Profiles', () => {
+	describe('GET /profiles/all', () => {
+		it('should get an array of profiles', (done) => {
+			chai
+				.request(app)
+				.get('/api/v1/profiles/all')
+				.end((err, res) => {
+					res.should.have.status(200);
+					const body = res.body;
+					body.should.be.a('array');
+					body.forEach((profile: Profile) => {
+						profile.should.haveOwnProperty('username');
+						profile.should.haveOwnProperty('joined');
+					});
+					done();
+				});
+		});
+	});
+});
