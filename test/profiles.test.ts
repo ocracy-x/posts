467d125676f3dc2a2b08cde699a39d21f67b6e37
William Z <williazz@usc.edu>
2022-05-08 14:31:19 -0400

    Wrote decent tests for all Create, Read, and Delete operations

--- test/profiles.test.ts
+++ test/profiles.test.ts
@@ -50,6 +50,19 @@ describe('Profiles', () => {
 					done();
 				});
 		});
+
+		it('should fail to write duplicate usernames', (done) => {
+			chai
+				.request(app)
+				.post('/api/v1/profiles')
+				.query({
+					username: 'test',
+				})
+				.end((_, res) => {
+					res.should.have.status(400);
+					done();
+				});
+		});
 	});
 
 	describe('DELETE /profile/:id', () => {
@@ -59,6 +72,16 @@ describe('Profiles', () => {
 				.delete('/api/v1/profiles/test')
 				.end((_, res) => {
 					res.should.have.status(200);
+					done();
+				});
+		});
+
+		it('should not have a profile with username test', (done) => {
+			chai
+				.request(app)
+				.get('/api/v1/profiles/test')
+				.end((_, res) => {
+					res.should.have.status(404);
 					done();
 				});
 		});
