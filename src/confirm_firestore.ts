4a6b2e3180deb28a71a4261e6dae581679531319
William Z <williazz@usc.edu>
2022-05-08 10:01:52 -0400

    Confirm firestore connection

---
+++ src/confirm_firestore.ts
@@ -0,0 +1,23 @@
+const { Storage } = require('@google-cloud/storage');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = new Storage();
+// Makes an authenticated API request.
+async function listBuckets() {
+	try {
+		const results = await storage.getBuckets();
+
+		const [buckets] = results;
+
+		console.log('Buckets:');
+		buckets.forEach((bucket: any) => {
+			console.log(bucket.name);
+		});
+	} catch (err) {
+		console.error('ERROR:', err);
+		process.exit();
+	}
+}
+listBuckets();
